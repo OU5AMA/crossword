e0c30ce18994e2038c964cf0c4e609dcb96a5a0d
OU5AMA <lasri.oussama@gmail.com>
2024-02-12 17:58:36 +0100

    create a backend part with express, dotenv, jsonwebtoken, and bcrypt

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const app = express();
+
+
+// Dummy users and puzzles data (replace with your my own database later)

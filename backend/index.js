94c4cc2b38db426d5290f76aae20031288750b24
OU5AMA <lasri.oussama@gmail.com>
2024-02-12 19:29:06 +0100

    Middleware for user authentication

--- backend/index.js
+++ backend/index.js
@@ -29,3 +29,21 @@ app.post('/login', async(req, res)=>{
         return res.status(401).json({message: 'Invalid password'});
     }
 });
+
+// Middleware for user authenticatation
+const autheticateUser = (req, res, next)=>{
+    const token = req.headers.authorization?.split(' ')[1];
+    if(!token){
+        return res.status(401).json({message: 'Unauthorized'});
+    }
+    
+    jwt.verify(token, 'secretkey', (err, decoded) => {
+        if(err){
+            return res.status(401).json({message: 'Invalid token'});
+        }
+        req.user = decoded;
+        next();
+    });
+};
+
+

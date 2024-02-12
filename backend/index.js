14127b7b175adbbca7b8e44448ca842d20e58b6b
OU5AMA <lasri.oussama@gmail.com>
2024-02-12 18:01:46 +0100

    add username

--- backend/index.js
+++ backend/index.js
@@ -5,3 +5,7 @@ const app = express();
 
 
 // Dummy users and puzzles data (replace with your my own database later)
+const users = [
+    { username: 'user', passwordHash: '$2b$10$BOPiW.Jzb/ZzavGuVsRJee.ks7xYGXrbhhDxX.3b6Y8l0qiq.JD2e' }, // password: password
+    { username: 'admin', passwordHash: '$2b$10$v3R5gIEqUoQaivVnt8YCTecdc8KJEXjwEZvHqGe1MSbUjX0miMBXq' } // password: adminpassword
+  ];

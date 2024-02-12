872e8c981cd10d40f4c3f480ccc5859b0f2d13e7
OU5AMA <lasri.oussama@gmail.com>
2024-02-12 18:07:34 +0100

    authenticate the user

--- backend/index.js
+++ backend/index.js
@@ -9,3 +9,23 @@ const users = [
     { username: 'user', passwordHash: '$2b$10$BOPiW.Jzb/ZzavGuVsRJee.ks7xYGXrbhhDxX.3b6Y8l0qiq.JD2e' }, // password: password
     { username: 'admin', passwordHash: '$2b$10$v3R5gIEqUoQaivVnt8YCTecdc8KJEXjwEZvHqGe1MSbUjX0miMBXq' } // password: adminpassword
   ];
+
+const puzzles = [
+    // example puzzle data
+]; 
+
+// Autheticate user
+app.post('/login', async(req, res)=>{
+    const {username , password}=req.body;
+    const user= users.find(user=>user.username === username);
+    if(!user){
+        return res.status(404).json({message: 'User Not found'});
+    }
+
+    if(await bcrypt.compare(passoword, user.passwordHash)){
+        const token = jwt.sign({username}, 'secretkey', {expiresIn: '1h'});
+        return res.json({token});
+    }else{
+        return res.status(401).json({message: 'Invalid password'});
+    }
+});
